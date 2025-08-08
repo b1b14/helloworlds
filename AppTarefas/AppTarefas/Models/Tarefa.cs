82390a05013baa632ff40eda44ab37ae65d127f8
b1b14 <anabeatrialuiz201@gmial.com>
2025-08-08 16:35:01 -0300

    Adiciona funcionalidade inicial de gestão de tarefas
    - Atualiza `_Layout.cshtml` para definir idioma como "pt-br" e
      adiciona link no menu para "Listas de Tarefas".
    - Cria `TarefasController` com ação `Index` que retorna uma view.
    - Adiciona classe `Tarefa` com propriedades para gerenciar tarefas.
    - Cria view `Index.cshtml` com título "Tarefas".

---
+++ AppTarefas/AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
